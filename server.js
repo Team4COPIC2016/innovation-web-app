c2652d4c0670011eee9ba705fa6bcc09f4f7a66b
Scottie Enriquez <Team4.COPIC2016@gmail.com>
2016-04-13 13:36:26 -0500

    Initial working server commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var stylus = require('stylus');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3030;
+var environment = process.env.NODE_ENV || 'development';
+var app = express();
+var compile = function(str, path) {
+	return stylus(str).set('filename', path)
+};
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(bodyParser());
+app.use(stylus.middleware(
+	{
+		src : __dirname + '/public',
+		compile : compile
+	}
+));
+//deliver static content from the public directory
+app.use(express.static(__dirname + '/public/'));
+app.set('port', port);
+//use server routing for partials so that the Jade view engine can resolve the HTML
+app.get('/partials/:partialPath', function(request, response){
+	response.render('partials/' + request.params.partialPath);
+});
+//use server routing to serve JSON for API
+app.get('*', function(request, response) {
+	response.json("Hello, World!");
+});
+app.listen(port);
