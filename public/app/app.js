0fa0393b9faef47ae27517aa78c158e59f7c7f79
Vanessa <Team4.COPIC2016@gmail.com>
2016-04-13 20:30:27 -0500

    added css styling

--- public/app/app.js
+++ public/app/app.js
@@ -6,9 +6,9 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
 		requireBase : false
 	});
 	$routeProvider.when('/', { templateUrl: '/partials/homepage.jade' })
-	.when('/tasks/', { templateUrl: '/partials/tasks.jade', controller: 'taskController' })
-	.when('/employees/', { templateUrl: '/partials/employees.jade', controller: 'addEmployeeController' })
-
+	.when('/tasks', { templateUrl: '/partials/tasks.jade', controller: 'addTaskController' })
+	.when('/employees', { templateUrl: '/partials/employees.jade', controller: 'addEmployeeController' })
+	.when('/projects', { templateUrl: '/partials/projects.jade', controller: 'addProjectController' })
 	.otherwise({ redirectTo: '/error/' });
 });
 
@@ -18,9 +18,35 @@ angular.module('app').controller('addEmployeeController', function($scope, $http
 	$scope.error = false;
 	$scope.submit = function() {
 		$scope.success = true;
-		/*$http.post('/api/employee', $scope.form, function(){
-			//return a success message
-		});*/
-		//console.log($scope.form);
+		$http.post('http://default-environment.85izygvzu6.us-west-2.elasticbeanstalk.com/api/employee', $scope.form).then(function(data) {
+			console.log(data);
+		});
+		console.log($scope.form);
 	};
+});
+angular.module('app').controller('addProjectController', function($scope, $http) {
+	$scope.form = {};
+	$scope.success = false;
+	$scope.error = false;
+	$scope.submit = function() {
+		$scope.success = true;
+		$http.post('http://default-environment.85izygvzu6.us-west-2.elasticbeanstalk.com/api/project', $scope.form).then(function(data) {
+			console.log(data);
+		});
+		console.log($scope.form);
+	};
+
+});
+angular.module('app').controller('addTaskController', function($scope, $http) {
+	$scope.form = {};
+	$scope.success = false;
+	$scope.error = false;
+	$scope.submit = function() {
+		$scope.success = true;
+		$http.post('http://default-environment.85izygvzu6.us-west-2.elasticbeanstalk.com/api/task', $scope.form).then(function(data) {
+			console.log(data);
+		});
+		console.log($scope.form);
+	};
+
 });
